80e681c001a567a726a250b0750722ed4e3bf6f3
him69 <74186786+him69@users.noreply.github.com>
2022-10-05 14:17:20 +0530

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,14 @@
+
+        
+
+
+   $(document).ready(function () {
+    $(".menu").click(function () {
+        $(".user-nav").toggle();
+    });
+});
+$(document).ready(function () {
+    $(".menu").click(function () {
+        $(".user-nav").addClass("animel-t-r");
+    });
+});
